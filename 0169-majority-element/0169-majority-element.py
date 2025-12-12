fc7714e71a98f4fa7e1c42acf50fca2aaf5a89df
HemanthkumarGudala <113345025+Hemanthkumar077@users.noreply.github.com>
2025-12-12 22:14:22 +0530

    Time: 15 ms (15.71%), Space: 19.5 MB (18.85%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        dict1 = {}
+        for i in range(len(nums)):
+            if nums[i] not in dict1 :
+                dict1[nums[i]] = 1
+            else :
+                dict1[nums[i]] += 1
+        for key , value in dict1.items():
+            if value >= len(nums)/2 :
+                return key 
